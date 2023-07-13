12f975c54b8d55062e82dbf98c3a237e7cd80f61
Zulema Orellana H <106454251+zuzu9627@users.noreply.github.com>
2023-07-13 16:44:04 +0200

    attach components

--- climateia_project/src/App.jsx
+++ climateia_project/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import Context from "./components/Context";
+import DefaultPrediction from "./components/DefaultPrediction";
+import ModifiedPrediction from "./components/ModifiedPrediction";
+import AdviceNTips from "./components/AdviceNTips";
+import Footer from "./components/Footer";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <Hero />
+        <Context />
+        <DefaultPrediction />
+        <ModifiedPrediction />
+        <AdviceNTips />
+        <Footer />
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
