b7105b5801df907e56821542866ab432ec04cb02
Zulema Orellana H <106454251+zuzu9627@users.noreply.github.com>
2023-07-13 15:09:28 +0200

    default parameters context

--- climateia_project/src/components/Context.jsx
+++ climateia_project/src/components/Context.jsx
@@ -2,7 +2,77 @@ import React from 'react'
 
 const Context = () => {
   return (
-    <div>Context</div>
+    <div className="flex flex-row gap-1 items-center mb-12 ml-32 mr-24">
+    <div className="self-start flex flex-col justify-between mt-6 gap-3 w-1/2">
+      <div className="text-justify text-sm font-['Corbel'] leading-[22.4px] text-[#757575] mr-px">
+        La sequía, escasez de lluvias y altas temperaturas son una
+        preocupación en Cataluña. Hay un drástico descenso en las
+        precipitaciones, reduciendo los recursos hídricos. Las altas
+        temperaturas agravadas por el cambio climático aumentan la
+        evaporación. Gráficos de barras mostrarán el declive de las
+        lluvias y su impacto en el suministro de agua. Es urgente abordar
+        la situación con gestión del agua eficiente, conciencia sobre su
+        uso y alternativas sostenibles para enfrentar la sequía y el
+        cambio climático en Cataluña.
+      </div>
+      <div className="flex flex-row ml-px gap-5 items-center">
+        <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-col gap-3 w-1/2 h-[329px] items-center px-5 py-4 rounded-[30px]">
+          <div className="text-center text-2xl font-['Corbel'] text-[#415c2e] bg-[transparent] mb-5 w-2/3 h-[16.99%]">
+            Business Idea Planning
+          </div>
+          <img
+            src="https://file.rendit.io/n/bLoTL5ohBcqPN1q5DGxx.png"
+            className="min-h-0 min-w-0"
+          />
+          <div className="text-center font-['Corbel'] leading-[22.4px] text-[#658e47] self-stretch mr-3 h-[12.9%]">
+            We present you a proposal and discuss niffty-gritty like
+          </div>
+        </div>
+        <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-col gap-3 w-1/2 h-[329px] items-center px-5 py-4 rounded-[30px]">
+          <div className="text-center text-2xl font-['Corbel'] text-[#415c2e] bg-[transparent] mb-5 w-2/3 h-[16.99%]">
+            Business Idea Planning
+          </div>
+          <img
+            src="https://file.rendit.io/n/bLoTL5ohBcqPN1q5DGxx.png"
+            className="min-h-0 min-w-0"
+          />
+          <div className="text-center font-['Corbel'] leading-[22.4px] text-[#658e47] self-stretch mr-3 h-[12.9%]">
+            We present you a proposal and discuss niffty-gritty like
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row mr-px gap-5 items-center">
+        <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-col gap-3 w-1/2 h-[329px] items-center px-5 py-4 rounded-[30px]">
+          <div className="text-center text-2xl font-['Corbel'] text-[#415c2e] bg-[transparent] mb-5 w-2/3 h-[16.99%]">
+            Business Idea Planning
+          </div>
+          <img
+            src="https://file.rendit.io/n/bLoTL5ohBcqPN1q5DGxx.png"
+            className="min-h-0 min-w-0"
+          />
+          <div className="text-center font-['Corbel'] leading-[22.4px] text-[#658e47] self-stretch mr-3 h-[12.9%]">
+            We present you a proposal and discuss niffty-gritty like
+          </div>
+        </div>
+        <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-col gap-3 w-1/2 h-[329px] items-center px-5 py-4 rounded-[30px]">
+          <div className="text-center text-2xl font-['Corbel'] text-[#415c2e] bg-[transparent] mb-5 w-2/3 h-[16.99%]">
+            Business Idea Planning
+          </div>
+          <img
+            src="https://file.rendit.io/n/bLoTL5ohBcqPN1q5DGxx.png"
+            className="min-h-0 min-w-0"
+          />
+          <div className="text-center font-['Corbel'] leading-[22.4px] text-[#658e47] self-stretch mr-3 h-[12.9%]">
+            We present you a proposal and discuss niffty-gritty like
+          </div>
+        </div>
+      </div>
+    </div>
+    <img
+      src="https://file.rendit.io/n/vgowFxEpwLSfBDecrhYT.png"
+      className="min-h-0 min-w-0"
+    />
+  </div>
   )
 }
 
