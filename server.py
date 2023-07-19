e5d27b49a9bb15cfbb53084289ae23e2d04e35e3
abyss0one <villarodri14@gmail.com>
2023-07-19 22:32:40 +0200

    error in prediction

--- server.py
+++ server.py
@@ -49,25 +49,36 @@ from datetime import datetime
 #         print(f"Error al cargar el modelo: {str(e)}")
 #         return None
 
-def predict(model, features,fecha_actual, fecha_prediction, datos):
+def predict(model, features,fecha_actual, fecha_prediction, datos, model_temp, model_hume, features_temp, feautres_hume):
     try:
         # Realizar las predicciones utilizando el modelo y los parámetros
         # Aquí debes implementar tu lógica de predicción basada en el modelo cargado
 
         # los dos modelos necesitan valor_anterior, mes
 
-        dict_models_temp = joblib.load("./models/all_temp_models.pkl")
-        dict_models_hume = joblib.load("./models/all_hume_models.pkl")
-
-        dict_features_temp = joblib.load("./models/all_temp_features.pkl")
-        dict_features_hume = joblib.load("./models/all_hume_features.pkl")
-
         #HACER PREDICCION DE TEMP Y HUME DESPUES DE LA PREDICCION DE PREC
 
         # Ejemplo de predicción ficticia
-        for i in range(fecha_prediction):
+        for i in range(int(fecha_prediction)):
+            print("stop 0")
             pred_poly = features.transform(datos)
-            prediction = model.predict()
+            prediction = model.predict(pred_poly)
+            print("stop 1")
+            print(type(fecha_actual.month))
+            datos_hume = [[datos[0][0], fecha_actual.month]]
+            datos_temp = [[datos[0][1], fecha_actual.month]]
+            aux = datos[0]
+            print("stop 2")
+            poly_temp = features_temp.transform(datos_temp)
+            pred_temp = model_temp.predict(poly_temp)
+            print("stop 3")
+            poly_hume = feautres_hume.transform(datos_hume)
+            pred_hume = model_hume.predict(poly_hume)
+            print("stop 4")
+
+            datos = [[pred_temp, pred_hume, aux[0], prediction, aux[1]]]
+            print(datos)
+
         
         return prediction
     except Exception as e:
@@ -84,20 +95,21 @@ if __name__ == "__main__":
         dict_models = joblib.load("./models/all_models.pkl")
         dict_features = joblib.load("./models/all_features.pkl")
         dict_comarques = joblib.load("./comarques.joblib")
-
-        print(dict_comarques)
-        print(dict_comarques[id_comarca])
+        dict_models_temp = joblib.load("./models/all_temp_models.pkl")
+        dict_models_hume = joblib.load("./models/all_hume_models.pkl")
+        dict_features_temp = joblib.load("./models/all_temp_features.pkl")
+        dict_features_hume = joblib.load("./models/all_hume_features.pkl")
 
         df = pd.read_csv("./datasets_models/data.csv")
         df['data_lectura'] = pd.to_datetime(df['data_lectura'])
 
         df = df.sort_values(by='data_lectura', ascending=False)
         fecha_actualizada = df.iloc[0]['data_lectura']
-        datos =  df.iloc[0][['temp', 'hume', 'temp_anterior', 'precip_anterior', 'hume_anterior']]
-
+        datos =  [df.iloc[0][['temp', 'hume', 'temp_anterior', 'precip_anterior', 'hume_anterior']].values]
+        print(datos)
         if dict_models is not None:
             # Realizar la predicción
-            prediction = predict(dict_models[dict_comarques[id_comarca]], dict_features[dict_comarques[id_comarca]], fecha_actualizada, fecha_prediction, datos)
+            prediction = predict(dict_models[dict_comarques[id_comarca]], dict_features[dict_comarques[id_comarca]], fecha_actualizada, fecha_prediction, datos, dict_models_temp[dict_comarques[id_comarca]], dict_models_hume[dict_comarques[id_comarca]], dict_features_temp[dict_comarques[id_comarca]], dict_features_hume[dict_comarques[id_comarca]])
             print("Predicción:", prediction)
     else:
         print("Por favor, especifique la ruta del modelo y los parámetros como argumentos.")
