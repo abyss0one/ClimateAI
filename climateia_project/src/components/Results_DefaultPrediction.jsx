318dda7afa10286c49088c2ff99a842890d34498
Zulema Orellana H <106454251+zuzu9627@users.noreply.github.com>
2023-07-21 11:56:01 +0200

    condition for SPI and generated a json for results

--- climateia_project/src/components/Results_DefaultPrediction.jsx
+++ climateia_project/src/components/Results_DefaultPrediction.jsx
@@ -1,101 +1,110 @@
+// CON CONDICIONAL DEL SPI
 import React from "react";
+import jsonData from "../data/datagenerated_defaultprediction.json"; // Importa el archivo JSON
 
 const Results_DefaultPrediction = () => {
-  const commonShadow =
-    "shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-center h-20  items-center rounded-[30px]";
-  const commonImg = "min-h-0 min-w-0 w-8";
-  const commonText =
-    "text-center font-Poppins text-[#415c2e] bg-[transparent] h-full";
+  let data = {}; // Inicializa los datos vacíos
 
+  try {
+    data = jsonData; // Intenta importar el archivo JSON
+  } catch (error) {
+    console.error("Error al cargar los datos:", error); // Muestra el mensaje de error en la consola
+  }
+  // Función para obtener el mensaje según el valor de SPI
+  const getSPIMessage = (spi) => {
+    if (spi >= 2.0 && spi <= 2.5) {
+      return "Condiciones de humedad extrema";
+    } else if (spi >= 1.5 && spi < 2.0) {
+      return "Condiciones de humedad fuerte";
+    } else if (spi >= 1.0 && spi < 1.5) {
+      return "Condiciones de humedad moderada";
+    } else if (spi >= -1.0 && spi < 1.0) {
+      return "Condiciones de normalidad";
+    } else if (spi >= -1.5 && spi < -1.0) {
+      return "Condiciones de sequía moderada";
+    } else if (spi >= -2.0 && spi < -1.5) {
+      return "Condiciones de sequía fuerte";
+    } else if (spi >= -2.5 && spi < -2.0) {
+      return "Condiciones de sequía extrema";
+    } else {
+      return "Valor de SPI no definido";
+    }
+  };
+  // Función para obtener el mensaje según el valor de SPI
+  const getTemperatureMessage = (precipitacion) => {
+    if (precipitacion / 24 < 2.0) {
+      return "Lluvias débiles";
+    } else if (precipitacion / 24 >= 2.0 && precipitacion / 24 <= 15) {
+      return "Lluvias moderadas";
+    } else if (precipitacion / 24 >= 15 && precipitacion / 24 <= 30) {
+      return "Lluvias fuertes";
+    } else if (precipitacion / 24 >= 30 && precipitacion / 24 <= 60) {
+      return "Lluvias muy fuertes";
+    } else if (precipitacion / 24 > 60) {
+      return "Torrenciales";
+    } else {
+      return "Intensidad de lluvia no definida";
+    }
+  };
   return (
     <>
-      <div className={commonShadow + " gap-3"}>
-        <img
-          src="https://file.rendit.io/n/YEd8w5sfU8i9gyOuN5qM.svg"
-          className={commonImg}
-        />
-        <div className="relative flex flex-col pb-5 w-[180px]">
-          <div className="text-center w-[180px] h-[42.03%] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
-            25 º C
+      {/* Condición para mostrar los datos solo si el archivo JSON existe */}
+      {Object.keys(data).length > 0 && (
+        <>
+          {/* Temperatura */}
+          <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-center h-20  items-center rounded-[30px] gap-3">
+            <img
+              src="https://file.rendit.io/n/YEd8w5sfU8i9gyOuN5qM.svg"
+              className="min-h-0 min-w-0 w-8"
+            />
+            <div className="relative flex flex-col pb-5 w-[180px]">
+              <div className="text-center w-[180px] h-[42.03%] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
+                {data.temperatura} º C
+              </div>
+              <div className="text-center font-Poppins text-[#415c2e] bg-[transparent] h-full text-xl font-bold">
+                Temperatura
+              </div>
+            </div>
           </div>
-          <div className={commonText + " text-xl font-bold"}>Temperatura</div>
-        </div>
-      </div>
 
-      <div className={commonShadow + " gap-1"}>
-        <img
-          src="https://file.rendit.io/n/sOBspRzRkGrffSNZxAIj.svg"
-          className="min-h-0 min-w-0 w-12 "
-        />
-        <div className="relative flex flex-col pb-5 w-[180px]">
-          <div className="text-center w-[180px] h-[42.03%] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
-            12 mm
+          {/* Precipitación */}
+          <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-center h-20  items-center rounded-[30px] gap-3">
+            <img
+              src="https://file.rendit.io/n/sOBspRzRkGrffSNZxAIj.svg"
+              className="min-h-0 min-w-0 w-12 "
+            />
+            <div className="relative flex flex-col pb-5 w-[180px]">
+              <div className="text-center w-[180px] h-[42.03%] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
+                {data.precipitacion} mm/h {getTemperatureMessage(data.precipitacion)}
+              </div>
+              <div className="text-center font-Poppins text-[#415c2e] bg-[transparent] h-full text-xl font-bold">
+                Precipitación
+              </div>
+            </div>
           </div>
-          <div className={commonText + " text-xl font-bold"}>
-            Precipitación
-          </div>
-        </div>
-      </div>
 
-      <div className={commonShadow + " pt-6"}>
-        <img
-          src="https://file.rendit.io/n/BFDKisltuTtE978bOMpq.svg"
-          className="min-h-0 min-w-0 self-start mt-2 w-8"
-        />
-        <div className="relative flex flex-col pb-5 w-48">
-          <div className="text-center w-48 h-[42.03%] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
-            1.4 %
+          {/* Sequía IPS */}
+          <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-center h-20  items-center rounded-[30px] gap-3">
+            <img
+              src="https://file.rendit.io/n/BFDKisltuTtE978bOMpq.svg"
+              className="min-h-0 min-w-0 self-start mt-2 w-8"
+            />
+            <div className="relative flex flex-col pb-5 w-48">
+              <div className="text-center w-48 h-[42.03%] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
+                {data.SPI} % {getSPIMessage(data.SPI)}
+              </div>
+              <div className="text-center font-Poppins text-[#415c2e] bg-[transparent] h-full text-xl font-bold">
+                Sequía IPS
+              </div>
+            </div>
           </div>
-          <div className={commonText + " text-xl font-bold"}>Sequía IPS</div>
-        </div>
-      </div>
+        </>
+      )}
+
+      {/* Si no se pueden cargar los datos o el archivo JSON no existe */}
+      {Object.keys(data).length === 0 && <div>Error: Datos no encontrados</div>}
     </>
   );
 };
 
 export default Results_DefaultPrediction;
-
-// // ORIGINAL CODE
-// <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-end gap-4 h-32 shrink-0 items-center rounded-[30px]">
-// <img
-//   src="https://file.rendit.io/n/YEd8w5sfU8i9gyOuN5qM.svg"
-//   className="min-h-0 min-w-0 w-8 shrink-0"
-// />
-// <div className="relative flex flex-col pb-5 w-[180px]">
-//   <div className="text-center w-[180px] h-[42.03%] font-['Poppins'] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
-//     25 º C
-//   </div>
-//   <div className="text-center text-2xl font-['Poppins'] text-[#415c2e] bg-[transparent] h-full">
-//     Temperatura
-//   </div>
-// </div>
-// </div>
-// <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-end gap-1 h-32 shrink-0 items-center rounded-[30px]">
-// <img
-//   src="https://file.rendit.io/n/sOBspRzRkGrffSNZxAIj.svg"
-//   className="min-h-0 min-w-0 w-12 shrink-0"
-// />
-// <div className="relative flex flex-col pb-5 w-[180px]">
-//   <div className="text-center w-[180px] h-[42.03%] font-['Poppins'] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
-//     12 mm
-//   </div>
-//   <div className="text-center text-2xl font-['Poppins'] text-[#415c2e] bg-[transparent] h-full">
-//     Precipitación
-//   </div>
-// </div>
-// </div>
-// <div className="shadow-[0px_10px_30px_0px_rgba(16,_51,_30,_0.2)] bg-white flex flex-row justify-end pt-6 gap-1 h-32 shrink-0 items-center rounded-[30px]">
-// <img
-//   src="https://file.rendit.io/n/BFDKisltuTtE978bOMpq.svg"
-//   className="min-h-0 min-w-0 self-start mt-2 w-8 shrink-0"
-// />
-// <div className="relative flex flex-col pb-5 w-48">
-//   <div className="text-center w-48 h-[42.03%] font-['Poppins'] font-bold leading-[22.4px] text-[#658e47] absolute top-10 left-0">
-//     1.4 %
-//   </div>
-//   <div className="text-center text-2xl font-['Poppins'] text-[#415c2e] bg-[transparent] h-full">
-//     Sequía IPS
-//   </div>
-// </div>
-// </div>
-// </div>
