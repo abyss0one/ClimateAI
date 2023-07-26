faeeb9a4fc7210f9b36501a737366e24bbcf1356
Zulema Orellana H <106454251+zuzu9627@users.noreply.github.com>
2023-07-26 03:54:46 +0200

    UPDATE Chatbot

--- climateia_project/src/components/Chatbot.jsx
+++ climateia_project/src/components/Chatbot.jsx
@@ -80,30 +80,148 @@
 // };
 
 // export default ChatBot;
+
+// CORRECIÓN DEL MSN DEL BTN Y OPCIONES
+// import React, { useState } from 'react';
+
+// const ChatBot = () => {
+//   const [messages, setMessages] = useState([]);
+//   const [showOptions, setShowOptions] = useState(false);
+//   const [showWeatherInput, setShowWeatherInput] = useState(false);
+//   const [zipCode, setZipCode] = useState('');
+//   const API_KEY = 'c9fb4d861d57aa1cada026a6ec424394'; // Reemplaza con tu API key de la API climatológica
+
+//   const handleHelloButtonClick = () => {
+//     setShowOptions(!showOptions);
+//     if (!showOptions) {
+//       setMessages([
+//         {
+//           text: '¡Hola! Bienvenido@ al asistente de ClimateAI. Mi misión es ayudarte con consultas relacionadas al clima como la predicción actual del lugar donde te encuentras y consejos sobre cómo cuidar el medio ambiente.',
+//           sender: 'bot'
+//         },
+//         {
+//           text: '¿Qué te gustaría hacer?',
+//           sender: 'bot'
+//         },
+//       ]);
+//     }
+//   };
+
+//   const handleOptionClick = (option) => {
+//     setMessages([...messages, { text: `Has seleccionado ${option}`, sender: 'user' }]);
+//     setZipCode('');
+//     setShowOptions(false);
+
+//     if (option === '1') {
+//       setShowWeatherInput(true);
+//     } else {
+//       setShowWeatherInput(false);
+//     }
+//   };
+
+//   const handleWeatherInputSubmit = () => {
+//     // Lógica para obtener la información del clima y mostrarla en el chat
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${API_KEY}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setMessages([...messages, { text: `El clima en ${data.name} es: ${data.weather[0].description}`, sender: 'bot' }]);
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching weather data:', error);
+//       });
+
+//     setShowWeatherInput(false);
+//   };
+
+//   return (
+//     <div className="fixed bottom-4 right-4 bg-white p-4 rounded-lg shadow-lg">
+//       {messages.map((message, index) => (
+//         <div
+//           key={index}
+//           className={`mb-2 ${message.sender === 'bot' ? 'text-green-500' : 'text-blue-500'} font-semibold`}
+//         >
+//           {message.text}
+//         </div>
+//       ))}
+//       {showOptions && (
+//         <div>
+//           <button
+//             onClick={() => handleOptionClick('1')}
+//             className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2 mr-2"
+//           >
+//             Clima ahora
+//           </button>
+//           <button
+//             onClick={() => handleOptionClick('2')}
+//             className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+//           >
+//             Consejos
+//           </button>
+//         </div>
+//       )}
+//       {!showOptions && (
+//         <button
+//           onClick={handleHelloButtonClick}
+//           className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+//         >
+//           Hello!
+//         </button>
+//       )}
+//       {showWeatherInput && (
+//         <div>
+//           <input
+//             type="text"
+//             value={zipCode}
+//             onChange={(e) => setZipCode(e.target.value)}
+//             className="border border-gray-300 rounded-lg p-2 mt-2"
+//             placeholder="Introduce tu código postal/ZIP"
+//           />
+//           <button
+//             onClick={handleWeatherInputSubmit}
+//             className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+//           >
+//             Obtener clima
+//           </button>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ChatBot;
+
+
 import React, { useState } from 'react';
 
 const ChatBot = () => {
-  const [messages, setMessages] = useState([{ text: 'Hola!', sender: 'bot' }]);
-  const [showOptions, setShowOptions] = useState(true);
+  const [messages, setMessages] = useState([]);
+  const [showOptions, setShowOptions] = useState(false);
   const [showWeatherInput, setShowWeatherInput] = useState(false);
   const [zipCode, setZipCode] = useState('');
-  const API_KEY = 'TU_API_KEY'; // Reemplaza con tu API key de la API climatológica
+  const API_KEY_GEOCODING = 'c9fb4d861d57aa1cada026a6ec424394'; // Reemplaza con tu API key de Geocoding
+  const API_KEY_WEATHER = 'c9fb4d861d57aa1cada026a6ec424394'; // Reemplaza con tu API key de la API climatológica
 
   const handleHelloButtonClick = () => {
     setShowOptions(!showOptions);
-    if (showOptions) {
+    if (!showOptions) {
       setMessages([
-        ...messages,
-        { text: '¿Qué te gustaría saber?', sender: 'bot' },
-        { text: '1. Clima ahora', sender: 'bot' },
-        { text: '2. Consejos', sender: 'bot' },
+        {
+          text: '¡Hola! Bienvenido@ al asistente de ClimateAI. Mi misión es ayudarte con consultas relacionadas al clima como la predicción actual del lugar donde te encuentras y consejos sobre cómo cuidar el medio ambiente.',
+          sender: 'bot'
+        },
+        {
+          text: '¿Qué te gustaría hacer?',
+          sender: 'bot'
+        },
       ]);
     }
   };
 
   const handleOptionClick = (option) => {
     setMessages([...messages, { text: `Has seleccionado ${option}`, sender: 'user' }]);
-    setZipCode(option);
+    setZipCode('');
+    setShowOptions(false);
+
     if (option === '1') {
       setShowWeatherInput(true);
     } else {
@@ -111,9 +229,47 @@ const ChatBot = () => {
     }
   };
 
+  const getCoordinatesFromAddress = () => {
+    const address = zipCode; // Usaremos el código postal introducido por el usuario como dirección
+
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=08940,ES&appid=c9fb4d861d57aa1cada026a6ec424394`;
+    // http://api.openweathermap.org/geo/1.0/zip?zip=08940,ES&appid=c9fb4d861d57aa1cada026a6ec424394
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.length > 0) {
+          const { lat, lon } = data[0];
+          // Aquí puedes usar lat y lon (latitud y longitud) para realizar la consulta del clima en la API climatológica
+          console.log('Latitud:', lat);
+          console.log('Longitud:', lon);
+          // Llama a la función para obtener el clima usando las coordenadas (lat, lon)
+          getWeatherData(lat, lon);
+        } else {
+          console.error('No se encontraron coordenadas para la dirección proporcionada.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching coordinates:', error);
+      });
+  };
+
+  const getWeatherData = (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY_WEATHER}`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // Aquí puedes hacer lo que desees con los datos climáticos obtenidos
+        console.log('Datos climáticos:', data);
+        setMessages([...messages, { text: `El clima en ${data.name} es: ${data.weather[0].description}`, sender: 'bot' }]);
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
+      });
+  };
+
   const handleWeatherInputSubmit = () => {
-    // Lógica para obtener la información del clima y mostrarla en el chat
-    // ...
+    getCoordinatesFromAddress(); // Llamada a la función para obtener las coordenadas antes de consultar el clima
     setShowWeatherInput(false);
   };
 
@@ -128,14 +284,6 @@ const ChatBot = () => {
         </div>
       ))}
       {showOptions && (
-        <button
-          onClick={handleHelloButtonClick}
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
-        >
-          Hello!
-        </button>
-      )}
-      {!showOptions && (
         <div>
           <button
             onClick={() => handleOptionClick('1')}
@@ -150,6 +298,14 @@ const ChatBot = () => {
             Consejos
           </button>
         </div>
+      )}
+      {!showOptions && (
+        <button
+          onClick={handleHelloButtonClick}
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg mt-2"
+        >
+          Hello!
+        </button>
       )}
       {showWeatherInput && (
         <div>
